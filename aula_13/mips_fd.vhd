715f5274bcbe0d038881f26afc9df92ea488eb6f
cicerotcv <elm.tiago@gmail.com>
2021-10-20 01:17:51 -0300

    init aula_13

---
+++ aula_13/mips_fd.vhd
@@ -0,0 +1,17 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity mips_fd is
+    generic ( 
+    );
+    port
+    ( 
+    );
+end entity;
+
+architecture comportamento of mips_fd is
+
+begin
+
+end architecture;
